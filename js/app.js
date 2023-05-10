47e901ccb1f2635311b5c853d7bec96293abfa10
alvaro valencia <alvaroleandro74@gmail.com>
2023-05-10 01:24:54 -0300

    limites del snake, trazado del mapa, modificacion de coordenadas, colision del snake y la manzana

--- js/app.js
+++ js/app.js
@@ -1,35 +1,71 @@
 var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
 var x = 0;
 var y = 0;
+var random = 0;
+var flag = true;
+
+/* Scenario */
+for (i = 0; i < 40; i++) {
+    ctx.beginPath();
+    ctx.setLineDash([]);
+    ctx.moveTo(0, i * 20);
+    ctx.lineTo(800, i * 20);
+
+    ctx.moveTo(i * 20, 0);
+    ctx.lineTo(i * 20, 500);
+    ctx.stroke();
+}
 
 if (canvas.getContext) {
-    var ctx = canvas.getContext('2d');
 
     /* Number random */
     function getRandomInt(max) {
-        return Math.floor(Math.random() * max);
+        random = (Math.floor(Math.random() * max) * 100)
+        if (random == 0) {
+            random = (Math.floor(Math.random() * max) * 100)
+        }   
+
+        return random;
     }
 
     /* Snake */
     const snake = {
-        x: getRandomInt(800),   // begin
-        y: getRandomInt(500),
+        x: getRandomInt(8),   // begin
+        y: getRandomInt(5),
         width: 20,
         color: '#018301',
         dead: false,
         speed: 1000,
         draw(x, y) {
+
             ctx.beginPath();
-            ctx.fillStyle = '#FFDA01';
-            ctx.fillRect(0, 0, 800, 500);
+            ctx.lineTo(x, y)
             ctx.strokeStyle = this.color;
             ctx.lineWidth = this.width;
-            ctx.lineCap = 'round';
+            ctx.lineCap = 'square';
             ctx.lineJoin = 'round';
-            ctx.lineTo(x, y);
+            ctx.moveTo(x, y);
             ctx.stroke();
             limit();
+            apple.draw();
         },
+    }
+
+    /* Apple */
+    const apple = {
+        color: 'red',
+        width: 20,
+        x: getRandomInt(7),   // begin
+        y: getRandomInt(4),
+        draw(x, y) {
+            ctx.beginPath();
+            ctx.strokeStyle = this.color;
+            ctx.lineTo(this.x, this.y);
+            ctx.lineWidth = this.width;
+            ctx.lineCap = 'round';
+            ctx.stroke();
+        }
     }
 
     /* Direcciones */
@@ -37,16 +73,16 @@ if (canvas.getContext) {
 
         switch (e.key) {
             case 'ArrowDown':
-                snake.draw(snake.x, snake.y += 10.5);
+                snake.draw(snake.x, snake.y += 20);
                 break;
             case 'ArrowUp':
-                snake.draw(snake.x, snake.y -= 10.5);
+                snake.draw(snake.x, snake.y -= 20);
                 break;
             case 'ArrowLeft':
-                snake.draw(snake.x -= 10.5, snake.y);
+                snake.draw(snake.x -= 20, snake.y);
                 break;
             case 'ArrowRight':
-                snake.draw(snake.x += 10.5, snake.y);
+                snake.draw(snake.x += 20, snake.y);
                 break;
         }
 
@@ -54,7 +90,7 @@ if (canvas.getContext) {
 
     /* Limites */
     const limit = () => {
-        if (snake.y < 0 || snake.y > canvas.height || snake.x < 0 || snake.x > canvas.width - 10.5) {
+        if (snake.y <= 0 || snake.y >= canvas.height || snake.x <= 0 || snake.x >= canvas.width) {
             ctx.strokeStyle = 'red';
             snake.dead = true;
             console.log('fuera de juego');
@@ -62,12 +98,18 @@ if (canvas.getContext) {
         if (snake.dead) {
             alert('juego terminado');
             snake.dead = false;
-            snake.x = getRandomInt(800);
-            snake.y = getRandomInt(500);
+            snake.x = getRandomInt(8);
+            snake.y = getRandomInt(5);
         }
+
+        if ((apple.x >= snake.x && snake.x >= apple.x) && (apple.y >= snake.y && snake.y >= apple.y)) {
+
+            console.log('COLISION');
+        }
+
     }
 
-    
+
 } else {
     /* unnsoported code */
     document.write('<h1>This site not support canvas. please retry in another dispositive</h1>')
