ab1b2c1e2917528353c792d871443df94e0df536
alvaro valencia <alvaroleandro74@gmail.com>
2023-05-16 00:45:16 -0300

    agregando velocidad por animationFrame

--- js/app.js
+++ js/app.js
@@ -3,31 +3,71 @@ var ctx = canvas.getContext('2d');
 var x = 0;
 var y = 0;
 var random = 0;
-var flag = true;
+var num = 0;
+var keyPress = '';
+var numero = 0;
+var flag = false;
+var request;
+var intervalo = 1000;
+var drawFlag = false;
+var ejeX;
+var izq;
+var arriba;
 
 /* Scenario */
-for (i = 0; i < 40; i++) {
+/* for (i = 0; i < 40; i++) {
     ctx.beginPath();
     ctx.setLineDash([]);
     ctx.moveTo(0, i * 20);
     ctx.lineTo(800, i * 20);
-
+    
     ctx.moveTo(i * 20, 0);
     ctx.lineTo(i * 20, 500);
     ctx.stroke();
+} */
+
+/* Number random */
+function getRandomInt(max) {
+    random = (Math.floor(Math.random() * max) * 100)
+    if (random == 0) {
+        random = (Math.floor(Math.random() * max) * 100)
+    }
+
+    return random;
 }
 
 if (canvas.getContext) {
+    
+    /* Direcciones */
+    window.addEventListener('keydown', (e) => {    
+        e.key == Number(e.key) ? console.log('es un numero') : (keyPress = e.key);
+        
+        //console.log('se presiono una tecla',keyPress); 
+        if (keyPress == 'p') {
+            cancelAnimationFrame(request)  
+        }
+        if (keyPress == 's') {
+            dibujar()
+        }
 
-    /* Number random */
-    function getRandomInt(max) {
-        random = (Math.floor(Math.random() * max) * 100)
-        if (random == 0) {
-            random = (Math.floor(Math.random() * max) * 100)
-        }   
-
-        return random;
-    }
+        /* movimientos */
+        if (keyPress == 'ArrowDown') {
+            ejeX = false;
+            arriba = false;
+        }
+        if (keyPress == 'ArrowUp') {
+            ejeX = false;
+            arriba = true;
+        }
+        if (keyPress == 'ArrowLeft') {
+            ejeX = true;
+            izq = true;
+        }
+        if (keyPress == 'ArrowRight') {
+            ejeX = true;
+            izq = false;
+        }
+    })
 
     /* Snake */
     const snake = {
@@ -36,20 +76,7 @@ if (canvas.getContext) {
         width: 20,
         color: '#018301',
         dead: false,
-        speed: 1000,
-        draw(x, y) {
-
-            ctx.beginPath();
-            ctx.lineTo(x, y)
-            ctx.strokeStyle = this.color;
-            ctx.lineWidth = this.width;
-            ctx.lineCap = 'square';
-            ctx.lineJoin = 'round';
-            ctx.moveTo(x, y);
-            ctx.stroke();
-            limit();
-            apple.draw();
-        },
+        velocity: 60,
     }
 
     /* Apple */
@@ -58,58 +85,83 @@ if (canvas.getContext) {
         width: 20,
         x: getRandomInt(7),   // begin
         y: getRandomInt(4),
-        draw(x, y) {
-            ctx.beginPath();
-            ctx.strokeStyle = this.color;
-            ctx.lineTo(this.x, this.y);
-            ctx.lineWidth = this.width;
-            ctx.lineCap = 'round';
-            ctx.stroke();
-        }
     }
-
-    /* Direcciones */
-    window.addEventListener('keydown', (e) => {
-
-        switch (e.key) {
-            case 'ArrowDown':
-                snake.draw(snake.x, snake.y += 20);
-                break;
-            case 'ArrowUp':
-                snake.draw(snake.x, snake.y -= 20);
-                break;
-            case 'ArrowLeft':
-                snake.draw(snake.x -= 20, snake.y);
-                break;
-            case 'ArrowRight':
-                snake.draw(snake.x += 20, snake.y);
-                break;
-        }
-
-    })
-
+    
     /* Limites */
-    const limit = () => {
+    function limit (){
         if (snake.y <= 0 || snake.y >= canvas.height || snake.x <= 0 || snake.x >= canvas.width) {
             ctx.strokeStyle = 'red';
             snake.dead = true;
             console.log('fuera de juego');
+            
         }
         if (snake.dead) {
             alert('juego terminado');
             snake.dead = false;
             snake.x = getRandomInt(8);
             snake.y = getRandomInt(5);
+            
         }
-
         if ((apple.x >= snake.x && snake.x >= apple.x) && (apple.y >= snake.y && snake.y >= apple.y)) {
-
             console.log('COLISION');
+            apple.x = getRandomInt(8);
+            apple.y = getRandomInt(5);
+            snake.velocity -= 10;
+            //snake.color = 'red'
         }
-
     }
 
+    
+    const drawSnake = () => {
+        ctx.lineTo(snake.x, snake.y)
+        //ctx.moveTo(snake.x, snake.y);
+        
+        ctx.strokeStyle = snake.color;
+        ctx.lineWidth = snake.width;
+        ctx.lineCap = 'square';
+        ctx.lineJoin = 'round';
 
+        drawFlag = true;
+        /* console.log('se dibuja el snake'); */
+    }
+
+    const drawApple = () => {          
+        ctx.lineTo(apple.x, apple.y);
+
+        ctx.lineWidth = apple.width;
+        ctx.strokeStyle = apple.color;
+        ctx.lineCap = 'round';
+
+        drawFlag = false;
+        /* console.log('se dibuja la manzana'); */
+    }
+
+    /* draw and speed */
+    function dibujar () {
+        ++num;
+        ctx.beginPath();   
+        drawFlag ? drawApple():drawSnake();
+
+        if (num == snake.velocity) {
+
+            if (ejeX) {
+                izq ? snake.x-=20 : snake.x+=20; 
+            }
+            if (!ejeX) {
+                arriba ? snake.y-=20 : snake.y+=20;
+            }
+            console.log(snake.velocity , 'velocidad snake ');
+            num = 0;
+        }
+        
+        ctx.stroke();
+        console.log('me estoy dibujado');
+        limit();
+
+        request = requestAnimationFrame(dibujar)
+    }
+    
+    
 } else {
     /* unnsoported code */
     document.write('<h1>This site not support canvas. please retry in another dispositive</h1>')
